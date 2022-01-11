0955363fbfbb8fdfb8876c1ec71a90f7fc2282ea
Jean <jean.carlosmolossi@gmail.com>
2022-01-10 22:53:04 -0300

    refactor: removido Mocktable

--- dynamo-table.go
+++ dynamo-table.go
@@ -25,15 +25,6 @@ type (
 	}
 
 	TableMetadata map[string]interface{}
-
-	Mocktable struct {
-		PK           string `diinamo:"type:string;hash"`
-		SK           string `diinamo:"type:string;range"`
-		Owner        string `diinamo:"type:string;gsi:CourseOwnerIndex;keyPairs:PK=Owner"`
-		Title        string `diinamo:"type:string;gsi:CourseTitleIndex;keyPairs:Title=SK"`
-		ParentCourse string `diinamo:"type:string;gsi:CourseLessonsIndex;keyPairs:ParentCourse=SK"`
-		ParentModule string `diinamo:"type:string;gsi:ModuleLessonsIndex;keyPairs:ParentModule=SK"`
-	}
 )
 
 const (
