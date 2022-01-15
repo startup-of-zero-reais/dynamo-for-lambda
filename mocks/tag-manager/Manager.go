509686f1fc76e682a61c1db55de1aa9ef85aeced
Jean <jean.carlosmolossi@gmail.com>
2022-01-14 21:36:37 -0300

    test: adicionados mocks auto gerados

--- mocks/tag-manager/Manager.go
+++ mocks/tag-manager/Manager.go
@@ -12,6 +12,34 @@ type Manager struct {
 	mock.Mock
 }
 
+// GetHash provides a mock function with given fields:
+func (_m *Manager) GetHash() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+
+// GetRange provides a mock function with given fields:
+func (_m *Manager) GetRange() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+
 // MapTags provides a mock function with given fields:
 func (_m *Manager) MapTags() error {
 	ret := _m.Called()
